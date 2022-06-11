e8b24308d5acf08c11b6b92856a78369462968ee
nayanika2304 <nayanikabhargava1993@gmail.com>
2022-06-11 17:37:24 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
+import MapboxWorker from 'mapbox-gl/dist/mapbox-gl-csp-worker';
 
 const MAPBOX_TOKEN ='pk.eyJ1IjoibmIyMzA0IiwiYSI6ImNrdXppeHp0bjIxc3oyd3A2dDlyM2Y4dWgifQ.AN1SVHhfWVXUYtAhOjCbOg'
 
+mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = MAPBOX_TOKEN
+
 
 const App = () =>{
   const mapContainer = useRef(null);
